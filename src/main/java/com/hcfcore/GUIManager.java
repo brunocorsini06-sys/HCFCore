8981aca5abbe175b6d21d8b40cc2c14cde1f8c3b
brunocorsini06-sys <bruno.corsini06@icloud.com>
2026-09-07 14:39:13 -0300

    Actualizar GUIManager.java

--- src/main/java/com/hcfcore/GUIManager.java
+++ src/main/java/com/hcfcore/GUIManager.java
@@ -16,107 +16,68 @@ public class GUIManager {
 
         this.plugin = plugin;
 
-        this.editorGUI =
-                new EditorGUI(plugin);
-
-        this.airdropEditorGUI =
-                new AirdropEditorGUI(plugin);
-
-        this.kitEditorGUI =
-                new KitEditorGUI(plugin);
-
-        this.kothEditorGUI =
-                new KothEditorGUI(plugin);
-
-        this.villagerEditorGUI =
-                new VillagerEditorGUI(plugin);
+        this.editorGUI = new EditorGUI(plugin);
+        this.airdropEditorGUI = new AirdropEditorGUI(plugin);
+        this.kitEditorGUI = new KitEditorGUI(plugin);
+        this.kothEditorGUI = new KothEditorGUI(plugin);
+        this.villagerEditorGUI = new VillagerEditorGUI(plugin);
     }
 
     public void register() {
 
-        plugin.getServer()
-                .getPluginManager()
-                .registerEvents(
-                        editorGUI,
-                        plugin
-                );
+        plugin.getServer().getPluginManager()
+                .registerEvents(editorGUI, plugin);
 
-        plugin.getServer()
-                .getPluginManager()
-                .registerEvents(
-                        airdropEditorGUI,
-                        plugin
-                );
+        plugin.getServer().getPluginManager()
+                .registerEvents(airdropEditorGUI, plugin);
 
-        plugin.getServer()
-                .getPluginManager()
-                .registerEvents(
-                        kitEditorGUI,
-                        plugin
-                );
+        plugin.getServer().getPluginManager()
+                .registerEvents(kitEditorGUI, plugin);
 
-        plugin.getServer()
-                .getPluginManager()
-                .registerEvents(
-                        kothEditorGUI,
-                        plugin
-                );
+        plugin.getServer().getPluginManager()
+                .registerEvents(kothEditorGUI, plugin);
 
-        plugin.getServer()
-                .getPluginManager()
-                .registerEvents(
-                        villagerEditorGUI,
-                        plugin
-                );
+        plugin.getServer().getPluginManager()
+                .registerEvents(villagerEditorGUI, plugin);
     }
 
     public void openMain(Player player) {
-
         editorGUI.openMain(player);
     }
 
     public void openAirdrop(Player player) {
-
         airdropEditorGUI.open(player);
     }
 
     public void openKit(Player player) {
-
         kitEditorGUI.open(player);
     }
 
     public void openKoth(Player player) {
-
         kothEditorGUI.open(player);
     }
 
     public void openVillager(Player player) {
-
         villagerEditorGUI.open(player);
     }
 
     public EditorGUI getEditorGUI() {
-
         return editorGUI;
     }
 
     public AirdropEditorGUI getAirdropEditorGUI() {
-
         return airdropEditorGUI;
     }
 
     public KitEditorGUI getKitEditorGUI() {
-
         return kitEditorGUI;
     }
 
     public KothEditorGUI getKothEditorGUI() {
-
         return kothEditorGUI;
     }
 
     public VillagerEditorGUI getVillagerEditorGUI() {
-
         return villagerEditorGUI;
     }
 }
